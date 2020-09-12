a65e4cbc861834d6215431aa4c9a9b420a0492ab
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:38:22 +0200

    Use class syntax
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var path = require('path');
 
-exports.Plugin = function (config) {
-    config.commands.export.option('minZoom', {
-        help: 'Min zoom to be considered for export',
-        metavar: 'INT',
-        default: 0
-    });
-    config.commands.export.option('maxZoom', {
-        help: 'Max zoom to be considered for export',
-        metavar: 'INT',
-        default: 18
-    });
-    config.registerExporter('mbtiles', path.join(__dirname, 'Export.js'));
-};
+class Plugin {
+    constructor(config) {
+        config.commands.export.option('minZoom', {
+            help: 'Min zoom to be considered for export',
+            metavar: 'INT',
+            default: 0
+        });
+        config.commands.export.option('maxZoom', {
+            help: 'Max zoom to be considered for export',
+            metavar: 'INT',
+            default: 18
+        });
+        config.registerExporter('mbtiles', path.join(__dirname, 'Export.js'));
+    }
+}
+
+exports = module.exports = { Plugin };
